c4aa4e3d29c5ac95e81f57b67a92575d7d26b0de
MzDicky21 <susbluuran@gmail.com>
2025-11-15 19:28:50 +0700

    fix the conflict

--- support/menu.tsx
+++
@@ -1,9 +0,0 @@
-import { Home, Users, WashingMachine, Code, LayoutDashboard } from 'lucide-react';
-
-export const menuItems = [
-  { title: "Home", icon: Home, url: "/" },
-  { title: "Dashboard", icon: LayoutDashboard, url: "/info" },
-  { title: "Customers", icon: Users, url: "/customers" },
-  { title: "Laundry", icon: WashingMachine, url: "/laundries" },
-  { title: "Technology", icon: Code, url: "/technology" },
-];
